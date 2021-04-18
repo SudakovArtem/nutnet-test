fb319c9467187b3d1b95e55346b257fac77b5a09
ArtemSudakov <tyomik1000@gmail.com>
2021-04-18 10:11:11 +0300

    правки

--- source/js/modules/init-reviews.js
+++ source/js/modules/init-reviews.js
@@ -5,18 +5,18 @@ const initReviews = () => {
     return;
   }
 
-  const reviewItems = document.querySelectorAll('.review__item');
+  const reviewItems = document.querySelectorAll('.review__card');
   const reviewText = reviewBlock.querySelector('.review__text');
   const reviewName = reviewBlock.querySelector('.review__name');
   const reviewPosition = reviewBlock.querySelector('.review__position');
-  let activeItem = document.querySelector('.review__item--active');
+  let activeItem = document.querySelector('.review__card--active');
 
   reviewItems.forEach((item) => {
     item.addEventListener('click', ((evt) => {
       evt.preventDefault();
-      if (!item.classList.contains('review__item--active')) {
-        activeItem.classList.remove('review__item--active');
-        item.classList.add('review__item--active');
+      if (!item.classList.contains('review__card--active')) {
+        activeItem.classList.remove('review__card--active');
+        item.classList.add('review__card--active');
         activeItem = item;
         reviewBlock.style.opacity = '0';
         reviewBlock.addEventListener('transitionend', () => {
